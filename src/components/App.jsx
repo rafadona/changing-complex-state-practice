f67be83b9f38fd6f26bd7aa24b3426f0e725a6cc
Rafael Dona <rafa_dona@hotmail.com>
2021-01-28 19:57:41 -0300

    array deconstruct and events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,59 @@
 import React from "react";
 
 function App() {
-  const [fName, setfName] = React.useState("");
-  const [lName, setlName] = React.useState("");
+  // const [fName, setfName] = React.useState("");
+  // const [lName, setlName] = React.useState("");
+
+  // function handleChange(event) {
+  //   setfName(event.target.value);
+  // }
+
+  // function handleChangelName(event) {
+  //   setlName(event.target.value);
+  // }
+
+  const [fullName, setFullName] = React.useState({
+    fName: "",
+    lName: ""
+  });
 
   function handleChange(event) {
-    setfName(event.target.value);
-  }
+    const { value, name } = event.target;
 
-  function handleChangelName(event) {
-    setlName(event.target.value);
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {fName} {lName}
+        Hello {fullName.fName} {fullName.lName}
       </h1>
       <form>
         <input
           onChange={handleChange}
           name="fName"
           placeholder="First Name"
-          value={fName}
+          value={fullName.fName}
         />
         <input
-          onChange={handleChangelName}
+          onChange={handleChange}
           name="lName"
           placeholder="Last Name"
-          value={lName}
+          value={fullName.lName}
         />
         <button>Submit</button>
       </form>
